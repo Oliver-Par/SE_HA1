704fd4e8c7a4094203895df72174e29f51bc3b52
Oliver-Par <parchatka.oliver@fh-swf.de>
2023-03-23 14:51:23 +0100

    Block 1
    Aufgaben Block 1 erledigt.

--- src/main/java/Rechnungen.java
+++ src/main/java/Rechnungen.java
@@ -1,12 +1,44 @@
-import javax.swing.*;
-import java.io.PrintStream;
+import java.io.OutputStream;
+import java.util.Arrays;
 
 public class Rechnungen {
-    public static String primzahl(int x){
-        return "";
+    public static boolean primzahl(int x){
+        int y = x - 1;
+        while (y > 1){
+            if(x % y == 0){
+                return false;
+            }
+            y--;
+        }
+        return true;
+    }
+
+    public static int quersumme(int x){
+        String zahl = x + "";
+        String[] zahlArray = zahl.split("");
+        int sum = 0;
+        for(int i = 0; i < zahlArray.length; i++){
+           sum += Integer.parseInt(zahlArray[i]);
+        }
+        return sum;
+    }
+
+    public static int testOneToHundret(){
+        int numbers = 0;
+        int x = 0;
+        for(int i = 1; i <= 100; i++){
+            if(primzahl(i)){
+                if(primzahl(quersumme(i))){
+                    numbers++;
+                }
+            }
+        }
+        return numbers;
     }
 
     public static void main(String[] args){
-        System.out.println(primzahl(3));
+        System.out.println(primzahl(113));
+        System.out.println(quersumme(1214552));
+        System.out.println(testOneToHundret());
     }
 }
