53592da476650437c4b94ee4b8b441afc19fd76d
Oliver-Par <parchatka.oliver@fh-swf.de>
2023-03-23 13:52:23 +0100

    Started

---
+++ src/main/java/Rechnungen.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.io.PrintStream;
+
+public class Rechnungen {
+    public static String primzahl(int x){
+        return "";
+    }
+
+    public static void main(String[] args){
+        System.out.println(primzahl(3));
+    }
+}
